c3133d708ccb47e39c64eeb623bc1d89f65ae2a7
swafvanSFK <safuswafvan1@gmail.com>
2024-11-14 20:53:52 +0530

    test

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import express, { Router } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import router from './routes/index.js';
-import connectDB from './config/db.js';
+import express, { Router } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes/index.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3030;
 const app = express();
 
-app.use(cors({
-    origin:"https://book-spot-bice.vercel.app",
-    methods : ["GET", "POST", "DELETE"]
-}));
+app.use(
+  cors({
+    origin: [
+      "https://book-spot-bice.vercel.app",
+      "http://localhost:5173", 
+    ],
+    methods: ["GET", "POST", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/', router);
+app.use("/", router);
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Connected to DB");
-        console.log("Running on port", PORT);
-    });
+  app.listen(PORT, () => {
+    console.log("Connected to DB");
+    console.log("Running on port", PORT);
+  });
 });
