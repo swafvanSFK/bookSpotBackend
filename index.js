a0bad0c7d8b4cf2197b55f82e58b54afd7211953
swafvanSFK <safuswafvan1@gmail.com>
2024-11-14 20:22:50 +0530

    for deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3030;
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://book-spot-bice.vercel.app",
     methods : ["GET", "POST", "DELETE"]
 }));
 app.use(express.json());
